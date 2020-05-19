c144933ac25330c9d2d75deed7d01909b1b7877a
chengyongchao <chengyc@meng-tu.com>
2020-05-19 17:30:52 +0800

    feat:日常提交

--- src/cyc/java/stream/Java8StreamAPI.java
+++ src/cyc/java/stream/Java8StreamAPI.java
@@ -1,14 +1,25 @@
 package cyc.java.stream;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -16,216 +27,257 @@ import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
 
+import cyc.java.collection.Vector;
+
 public class Java8StreamAPI
 {
-    public static void main(String[] args)
-    {
-        /****************** 获取流 ******************/
 
+    // ==================================获取流=============================================/
+
+    @Test
+    public void getStreamTest()
+    {
+        /**
+         * {@code Collection}在JDK1.8版本实现了Stream()方法和parallelStream()方法，
+         * 所有实现Collection的容器类通过调用stream()方法或者 parallelStream()
+         */
         // list
         List<String> list = new ArrayList<>();
         list.add("a");
         list.add("b");
         list.add("c");
         list.stream();
-        // 数组
-        String[] arr = {"a", "b", "c"};
-        String[] array2 = new String[] {};
-        String[] array3 = new String[6];
-        // 使用Arrays静态方法stream转换成流
-        Arrays.stream(arr);
 
-        // 值，直接将值转变成流对象
-        Stream<String> stream = Stream.of("a", "b", "c");
+        // Set
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.stream();
 
-        // Map，获取entrySet调用stream方法
+        // ArrayList
+        ArrayList<Integer> arrList = new ArrayList<>();
+        arrList.add(1);
+        arrList.add(2);
+        arrList.stream();
+
+        // Vector
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.stream();
+
+        // Queue
+        Queue<Integer> queue = new ArrayBlockingQueue<>(12);
+        queue.add(1);
+        queue.add(2);
+        queue.stream();
+
+        // Stack
+        Stack<Integer> stack = new Stack<>();
+        stack.add(1);
+        stack.add(2);
+        stack.stream();
+
+        // Map 通过调用map的entrySet()方法，获取Set再调用stream()
         Map<Integer, String> map = new HashMap<>();
         map.put(1, "a");
         map.put(2, "b");
-        map.put(3, "c");
-        map.put(4, "d");
         map.entrySet().stream();
-        // 创建并行流
-        map.entrySet().parallelStream();
-        // 文件
-        /*
-         * try(Stream lines =
-         * Files.lines(Paths.get(“文件路径名”),Charset.defaultCharset())){
-         * //可对lines做一些操作 }catch(IOException e){ }
+
+        /**
+         * {@code Arrays}在JDK1.8中加入了stream系列方法， 数组可以调用Arrays.stream(T []
+         * array)实现流，同时还有参数不同的stream函数用来截取数组
          */
+        // 数组
+        String[] arr = {"a", "b", "c"};
+        Arrays.stream(arr);
 
-        /****************** stream方法 ******************/
+        // 值，直接将值转变成流对象
+        Stream.of("a", "b", "c");
 
+        // 从IO获取流
+        try (
+            Stream<String> lines = Files.lines(Paths.get("文件路径名"), Charset.defaultCharset())
+        )
+        {
+            // 可对lines做一些操作
+        }
+        catch (IOException e)
+        {}
+    }
+
+    // =============================stream方法===============================//
+
+    /**
+     * stream常用方法
+     */
+    @Test
+    public void streamMethodTest()
+    {
         /**
          * @decription 过滤器filter
          * @param 一个返回true或者false的lambda表达式
          * @return 筛选出返回结果为true的元素
          */
-
-        int cout1 = (int)Arrays.stream(arr).filter(a ->
+        Stream.of(1, 2, 3).filter(a ->
         {
-            return a.equals("c");
+            return a.equals(1);
         }).count();
 
         /**
          * @description 去重
-         * @return 去除重复的元素后的流
+         * @return the new stream
          */
-        Arrays.stream(arr).distinct();
+        Stream.of(1, 2, 2, 3).distinct();
 
         /**
          * @decription 截取前n个元素
-         * @return 流
+         * @return the new stream
          */
-        Arrays.stream(arr).limit(2);
+        Stream.of(1, 2, 3).limit(2);
 
         /**
          * @decription 跳过前n个元素
+         * @return the new stream
          */
-        Arrays.stream(arr).skip(1l);
+
+        Stream.of(1, 2, 3).skip(2);
 
         /**
-         * @decription 映射
+         * @decription map() 一对一映射 输入一个元素，按照给定的规则处理该元素
          */
-
-        int cout2 = (int)Arrays.stream(arr).map(a ->
+        Stream.of("a", "b", "c").map(a ->
         {
-            List<String> li = new ArrayList<>();
-            li.add(a);
-            return li.toArray();
-        }).flatMap(Arrays::stream).map(ar ->
-        {
-            System.out.println(ar);
-            return ar;
-        }).count();
-        System.out.println(cout2);
+            return a.toUpperCase();
+        }).map(String::toLowerCase);
 
         /**
-         * @description 是否匹配任意元素
+         * @description flatMap() 将流扁平化，说人话就是将流中最基层的元素抽出来为单独的流，eg:
+         *              List<List<Integer>>
+         *              类型的，会把最底层的Integer抽取出来，和map()不同的是map()将List<Integer>视为一个流进行处理
          */
-
-        Arrays.stream(arr).anyMatch(a ->
+        // 写法1
+        Stream.of(Arrays.asList(1, 2), Arrays.asList(3, 4), Arrays.asList(5, 6, 7)).flatMap(Collection::stream).map(a ->
         {
-            return a.equals("b");
+            // 此时 a就只是Integer了
+            return a.byteValue();
+        });
+        // 写法2
+        Stream.of(Arrays.asList(1, 2), Arrays.asList(3, 4), Arrays.asList(5, 6, 7)).flatMap(l ->
+        {
+            return l.stream();
+        }).map(a ->
+        {
+            return a.byteValue();
+        });
+
+        /**
+         * @description 是否匹配任意元素,有任意一个符合为true
+         */
+        Stream.of(1, 2, 3).anyMatch(a ->
+        {
+            return a.equals(1);
         });
 
         /**
          * @description 是否匹配所有元素
          */
-
-        Arrays.stream(arr).allMatch(a ->
+        Stream.of(1, 2, 3).allMatch(a ->
         {
-            return a.equals("b");
+            return a.equals(1);
         });
 
         /**
          * @description 是否所有元素都不匹配
          */
-
-        Arrays.stream(arr).noneMatch(a ->
+        Stream.of(1, 2, 3).noneMatch(a ->
         {
-            return a.equals("b");
+            return a.equals(1);
         });
 
         /**
          * @description 从流中返回任意元素，返回结果放在Optional对象中(实测怎么感觉就是findFirst())
          */
-        Optional<String> op1 = Arrays.stream(arr).findAny();
+        Stream.of(1, 2, 3).findAny();
 
         /**
          * @description 返回第一个元素，返回结果放在Optional对象中
          */
-
-        Optional<String> op2 = Arrays.stream(arr).findFirst();
-        op2.ifPresent((a) ->
-        {
-            System.out.println(a);
-        });
-        op2.isPresent();
-        op2.orElse("dd");
+        Optional<Integer> res = Stream.of(Arrays.asList(1, 2), Arrays.asList(3, 4)).flatMap(Collection::stream)
+                .findFirst();
+        res.ifPresent(System.out::print);
 
         /**
          * @description reduce 自定义lambda表达式求和 以
          *              identity为初始值，对流中的所有元素执行lambda表达式求和
          */
-
-        String result = Arrays.stream(arr).reduce("begin:", (a, b) ->
+        Stream.of(1, 2, 3).reduce(0, (a, b) ->
         {
             return a + b;
         });
 
-        /****************** stream数值流 ******************/
-        // 将普通流转换成数值流
+    }
 
-        Arrays.stream(arr).mapToInt(a ->
-        {
-            return Integer.valueOf(a);
-        });
-        Arrays.stream(arr).mapToDouble(a ->
-        {
-            return Double.valueOf(a);
-        });
-        Arrays.stream(arr).mapToLong(a ->
-        {
-            return Long.valueOf(a);
-        });
+    // =============================stream数值流 ===============================//
+    /**
+     * @description 将Stream流转换成数值流 eg： IntStream、LongStream
+     */
+    @Test
+    public void test3()
+    {
+        Stream.of(1, 2, 3).mapToDouble(Double::valueOf);
+        Stream.of(1, 2, 3).mapToLong(Long::valueOf);
+    }
 
-        /**
-         * @description 数值流计算方法 max min sum
-         */
+    // =============================stream收集器 ===============================//
 
-        /****************** stream收集器 ******************/
-        // 规约： 求最值、求和、求平均值
+    @Test
+    public void test4()
+    {
         /**
          * @description:求数量
          */
-
-        long num1 = Arrays.stream(arr).count();
-        // 使用收集器
-        long num2 = stream.collect(Collectors.counting());
+        // 写法1
+        Stream.of(1, 2, 3).count();
+        // 写法2
+        Stream.of(1, 2, 3).collect(Collectors.counting());
 
         /**
          * @description:求最值,最大值最小值都类似，比较方法有comparingDouble、comparingInt、comparingLong、甚至自定义
          */
-        Optional<String> res = Arrays.stream(arr).collect(Collectors.maxBy(Comparator.comparingDouble(a ->
+        // 写法1
+        Optional<Integer> res1 = Stream.of(1, 2, 3, 3).max((x, y) ->
         {
-            String a1 = a;
-            char[] ch = a1.toCharArray();
-            return Double.valueOf((int)ch[0]);
+            return (x < y) ? -1 : ((x == y) ? 0 : 1);
+        });
+        System.out.println(res1.get());
+        // 写法2 依据数值流比较大小不需要参数
+        OptionalInt res2 = Stream.of(1, 2, 3, 3).mapToInt(a ->
+        {
+            return a;
+        }).max();
+        System.out.println(res2.getAsInt());
+        // 写法3 借助已有的Collectors.maxBy方法，没啥用，知道能这样写就好
+        Optional<Integer> res3 = Stream.of(1, 2, 3, 3).collect(Collectors.maxBy(Comparator.comparingInt(a ->
+        {
+            return a;
         })));
-        System.out.println(res.get());
-        /**
-         * @description:求和 方法也很多，类似求最值
-         */
-
-        int sum = Arrays.stream(arr).collect(Collectors.summingInt(a ->
-        {
-            String a1 = a;
-            char[] ch = a1.toCharArray();
-            return Integer.valueOf((int)ch[0]);
-        }));
+        System.out.println(res3.get());
 
         /**
-         * @description:求平均值,方法也很多，类似求最值
-         */
-
-        double avg = Arrays.stream(arr).collect(Collectors.averagingInt(a ->
-        {
-            String a1 = a;
-            char[] ch = a1.toCharArray();
-            return Integer.valueOf((int)ch[0]);
-        }));
-
-        /**
-         * Collectors.summarizingInt函数能一次性将最值、均值、总和、元素个数全部计算出来，并存储在对象IntSummaryStatisics中。
-         * 可以通过该对象的getXXX()函数获取这些值。
+         * @description:求和、求平均值 方法也很多，类似求最值
          */
 
         /****************** stream连接字符串 ******************/
         /**
          * @description:连接字符串，默认中间无连接符，可以在joining方法中加入连接符 joining("_")
          */
-        String str = Arrays.stream(arr).collect(Collectors.joining());
+        Stream.of("a", "b").collect(Collectors.joining());
+        Stream.of("a", "b").reduce("", (a, b) ->
+        {
+            return a + b;
+        });
 
         /**
          * @description:自定义规约操作
@@ -233,7 +285,7 @@ public class Java8StreamAPI
          * @param:2.对流中的数据进行处理
          * @param:3.对处理后的元素进行自定义规约操作
          */
-        Arrays.stream(arr).collect(Collectors.reducing("begin:", a ->
+        Stream.of("a", "b", "c").collect(Collectors.reducing("begin:", a ->
         {
             return a;
         }, (a, b) ->
@@ -245,7 +297,7 @@ public class Java8StreamAPI
         /**
          * @description:将元素按指定规则分组
          */
-        Map<String, List<String>> res1 = Arrays.stream(arr).collect(Collectors.groupingBy(a ->
+        Map<String, List<String>> group1 = Stream.of("a", "b", "c").collect(Collectors.groupingBy(a ->
         {
             if ("a".equals(a))
             {
@@ -261,8 +313,9 @@ public class Java8StreamAPI
                 return "isOthers";
             }
         }));
+
         // 可以在collect方法中添加参数，如再次分组或者统计每个分组的数量
-        Map<String, Long> res2 = Arrays.stream(arr).collect(Collectors.groupingBy((a) ->
+        Map<String, Long> group2 = Stream.of("a", "b", "c").collect(Collectors.groupingBy((a) ->
         {
             if ("a".equals(a))
             {
@@ -278,60 +331,34 @@ public class Java8StreamAPI
                 return "isOthers";
             }
         }, Collectors.counting()));
-
     }
 
-    private Map<String, String> map2;
-
-    private Integer[] arr;
-
-    @Before
-    public void before()
-    {
-        map2 = new HashMap<>();
-        map2.put("1", "1");
-        map2.put("2", "2");
-        map2.put("3", "3");
-        map2.put("4", "4");
-        map2.put("5", "5");
-
-        arr = new Integer[6];
-        IntStream.range(0, 5).forEach((i) ->
-        {
-            arr[i] = i;
-        });
-    }
-
+    /**
+     * 优雅的结束Stream流
+     */
     @Test
-    public void returnMap()
+    public void endStreamTest()
     {
-        // stream流以map格式返回
-        Map<String, String> map3 = new HashMap<>();
-        map2 = map2.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
-        System.out.println(map3);
+        // 返回Map
+        Stream.of("a", "b", "c").collect(Collectors.toMap(s -> s, s -> s));
+        // 返回 List
+        Stream.of("a", "b", "c").collect(Collectors.toList());
+        // 返回Set
+        Stream.of("a", "b", "c").collect(Collectors.toSet());
     }
 
-    @Test
-    public void ArrayAndMapToSet()
-    {
-        // stream流以set格式返回
-        Set<Integer> set2 = new HashSet<>();
-        set2 = Arrays.stream(arr).collect(Collectors.toSet());
-        set2 = map2.entrySet().stream().map(m ->
-        {
-            return Integer.parseInt(m.getValue());
-        }).collect(Collectors.toSet());
-        System.out.println(set2);
-    }
-
+    /**
+     * 对Map进行排序
+     */
     @Test
     public void sortedMap()
     {
+        Map<String, String> map = new LinkedHashMap<>();
         // 方法一
-        map2 = map2.entrySet().stream().sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+        map = map.entrySet().stream().sorted((a, b) -> b.getValue().compareTo(a.getValue()))
                 .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
         // 方法二（其实就是对方法一的封装）
-        map2 = map2.entrySet().stream().sorted(Map.Entry.<String, String> comparingByValue().reversed())
+        map = map.entrySet().stream().sorted(Map.Entry.<String, String> comparingByValue().reversed())
                 .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
 
     }
